e2aca47188638e736fd3bbe8fb447edc46f3698f
pianoman373 <jmholley373@gmail.com>
2022-03-25 18:37:20 -0500

    update gstreamer_pipeline

--- display.py
+++ display.py
@@ -1,7 +1,33 @@
 import cv2
 
+def gstreamer_pipeline(
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink drop=True"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 if __name__ == '__main__':
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
     cv2.namedWindow("video")
 
     if cam.isOpened():
