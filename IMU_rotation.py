5e5ee9f5e7d75fe9b17f9cb4843ea83a6b1fcf00
EyesOn <travman246@gmail.com>
2022-04-23 23:45:34 -0500

    fixed syntax error because I forgot to test my code like a fool

--- IMU_rotation.py
+++ IMU_rotation.py
@@ -7,10 +7,6 @@ import numpy as np
 i2c = board.I2C()
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
-# If you are going to use UART uncomment these lines
-# uart = board.UART()
-# sensor = adafruit_bno055.BNO055_UART(uart)
-
 last_val = 0xFFFF
 
 def rotationMatrix():
@@ -20,11 +16,15 @@ def rotationMatrix():
                    [0, np.sin(eAngles[0]), np.cos(eAngles[0])]])
     Ry = np.array([[np.cos(eAngles[1]), 0, np.sin(eAngles[1])],
                    [0, 1, 0],
-                   [-1*np.sin(eAngles[1], 0, np.cos(eAngles[1])]])
+                   [-1*np.sin(eAngles[1]), 0, np.cos(eAngles[1])]])
     Rz = np.array([[np.cos(eAngles[2]), -1*np.sin(eAngles[2]), 0],
                    [np.sin(eAngles[2]), np.cos(eAngles[2]), 0],
                    [0, 0, 1]])
     R = np.dot(Rz, np.dot(Ry, Rx))
     return R
 
-
+if __name__ == "__main__":
+    while True:
+    	print("Euler angles: {}".format(sensor.euler))
+    	print("Rotation matrix: {}".format(rotationMatrix()))
+    	time.sleep(1)
