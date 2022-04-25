6d49eb7a42d16027542f69adb66eeef33b752d44
EyesOn <travman246@gmail.com>
2022-04-25 13:30:29 -0500

    IMU rotation matrix refinement

--- IMU_rotation.py
+++ IMU_rotation.py
@@ -8,20 +8,25 @@ i2c = board.I2C()
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 last_val = 0xFFFF
+prevR = np.eye(3)
 
 def rotationMatrix():
+    global prevR
     eAngles = sensor.euler
     Rx = np.array([[1, 0, 0],
                    [0, np.cos(eAngles[0]), -1*np.sin(eAngles[0])],
                    [0, np.sin(eAngles[0]), np.cos(eAngles[0])]])
-    Ry = np.array([[np.cos(eAngles[1]), 0, np.sin(eAngles[1])],
+    Ry = -1*np.array([[np.cos(eAngles[2]), 0, np.sin(eAngles[2])],
                    [0, 1, 0],
-                   [-1*np.sin(eAngles[1]), 0, np.cos(eAngles[1])]])
-    Rz = np.array([[np.cos(eAngles[2]), -1*np.sin(eAngles[2]), 0],
-                   [np.sin(eAngles[2]), np.cos(eAngles[2]), 0],
+                   [-1*np.sin(eAngles[2]), 0, np.cos(eAngles[2])]])
+    Rz = np.array([[np.cos(eAngles[1]), -1*np.sin(eAngles[1]), 0],
+                   [np.sin(eAngles[1]), np.cos(eAngles[1]), 0],
                    [0, 0, 1]])
     R = np.dot(Rz, np.dot(Ry, Rx))
-    return R
+    R_t = np.transpose(R)
+    Rdelta = np.dot(R_t, prevR)
+    prevR = R
+    return Rdelta
 
 if __name__ == "__main__":
     while True:
